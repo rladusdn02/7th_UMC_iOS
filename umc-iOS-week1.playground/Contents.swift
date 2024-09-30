8b2bc92db66658c67885b6fc7b138da2e038abe8
yeon <rladusdn56@gmail.com>
2024-09-30 13:40:23 +0900

    [Feat]: week1 문법 워크북 fin.

---
+++ umc-iOS-week1.playground/Contents.swift
@@ -0,0 +1,22 @@
+//문제 1
+let name: String = "김연우" //상수
+var age: Int = 23 //변수
+//문자열 보간법 사용
+let msg1: String = "이름: \(name) \n나이: \(age)"
+
+print(msg1)
+
+//문제 2
+let numA: Int = 10
+let numB: Double = 10.2
+let intVal: Int = Int(numB)
+
+let sum = numA + intVal
+let msg2: String = "결과 : \(sum)"
+print(msg2)
+
+//문제 3
+let name1: String = "김연우"
+let age1: Int = 23
+let msg3: String = "이름 : \(name1), 나이 : \(age1)"
+print(msg3)
